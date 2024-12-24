9bb8685e321f599ca48537b8833321739c7ed3df
somin <thals0313@naver.com>
2024-12-24 23:52:54 +0900

    cineshop

--- script/index.js
+++ script/index.js
@@ -1,3 +1,27 @@
+// const contents = document.querySelectorAll('.contents')
+// const popup = document.querySelectorAll('.popup > img')
+// const bg = document.querySelector('.popup_bg')
+
+// for(let i of popup){i.style.display = 'none'}
+
+// bg.style.display = 'none'
+
+// // promo
+// contents.forEach(function(t,i){
+//     t.addEventListener('click', function(e){
+//         e.preventDefault();
+//         popup[i].style.display = 'block'
+//         for(let i of popup){i.parentElement.style.display = 'block'}
+//         bg.style.display = 'block'
+//     })
+// })
+// bg.addEventListener('click',function(e){
+//     e.preventDefault()
+//     for(let i of popup){i.style.display = 'none'}
+//     for(let i of popup){i.parentElement.style.display = 'none'}
+//     this.style.display = 'none'
+// })
+
 // const moreBtn = document.querySelectorAll('.more')
 // const contents = document.querySelectorAll('.list > ul')
 
